31429efe3c03f69c6bb63faa8d52aa5fd6b9097d
Akshat Sinha <akshat.s@media.net>
2019-05-17 15:44:21 +0530

    remove timeout

--- whois.sh
+++ whois.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 ip=$1
-whois2=`gtimeout 5 whois $1`
+whois2=`whois $1`
 address=`echo $whois2 | grep -i "^address" | tail -n1 | awk -F ":" '{print $2}' | tr -d '\n' | awk '$1=$1' `
 city=`echo $whois2 | grep -i "^city" | tail -n1 | awk -F ":" '{print $2}' | tr -d '\n' | awk '$1=$1'`
 country=`echo $whois2 | grep -i "^country" | tail -n1 | awk -F ":" '{print $2}' | tr -d '\n' | awk '$1=$1'`
