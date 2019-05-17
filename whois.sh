9e6a9f5ae13538653fe98132f67202f00c783790
Akshat Sinha <akshat.s@media.net>
2019-05-17 15:06:14 +0530

    first

---
+++ whois.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+ip=$1
+whois2=`gtimeout 5 whois $1`
+address=`echo $whois2 | grep -i "address:" | tail -n1 | awk -F ":" '{print $2}' | tr -d '\n' | awk '$1=$1' `
+city=`echo $whois2 | grep -i "city:" | tail -n1 | awk -F ":" '{print $2}' | tr -d '\n' | awk '$1=$1'`
+country=`echo $whois2 | grep -i "country:" | tail -n1 | awk -F ":" '{print $2}' | tr -d '\n' | awk '$1=$1'`
+org=`echo $whois2 | grep -i "organization:" | tail -n1 | awk -F ":" '{print $2}' | tr -d '\n' | awk '$1=$1'`
+echo "$ip,$address,$city,$country,$org" >> "info.csv"
+# echo "$address"
